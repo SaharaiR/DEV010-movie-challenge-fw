5543cc45bd1a886160aa28cb159b945bd1e6f6a2
Sahara <sahararod@gmail.com>
2023-11-20 20:07:15 -0600

    se crearon los componentes del resto de la maquetacion (menuBar, gridMovies, navBar) se presentan las primeras 20 peliculas (page 1), actualizacion del changelog

--- src/connectAPI.ts
+++ src/connectAPI.ts
@@ -1,12 +1,17 @@
-// import React from "react";
-
 const API_KEY = 'ccedb42a54c3ee36b2d505f569a4b95f';
 
 interface Movie {
     title: string;
+    poster_path: string;
+    release_date: string;
 }
 
-export const getAnimatedMovies = (): Promise<Movie[]> => {
+interface MovieResponse{
+    page: number;
+    results: Movie[];
+}
+
+export const getAnimatedMovies = (): Promise<MovieResponse> => {
     return fetch(`https://api.themoviedb.org/3/discover/movie?with_genres=16&api_key=${API_KEY}`) 
-      .then(response => response.json())
+      .then(response => response.json() as unknown as MovieResponse)
 }
