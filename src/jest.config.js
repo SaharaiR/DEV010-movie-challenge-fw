a80dd73760e185c795aa27344d8234faea76a38e
Sahara <sahararod@gmail.com>
2024-01-16 12:33:06 -0600

    agregue las estrellitas del diseño

---
+++ src/jest.config.js
@@ -0,0 +1,6 @@
+export default {
+    preset: 'ts-jest',
+    testEnvironment: 'jsdom',
+    testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.[jt]sx?$',
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  };
